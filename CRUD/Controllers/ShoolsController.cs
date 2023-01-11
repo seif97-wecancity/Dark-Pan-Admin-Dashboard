fb9e6435463aa4fd4d65fbc90d77b5e7325c2927
seif97-wecancity <saif.elfagal@wecancity.org>
2023-01-11 17:13:39 +0200

    second commit

---
+++ CRUD/Controllers/ShoolsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUD.Models;
+namespace CRUD.Controllers
+{
+    public class ShoolsController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            List<School> Schools = new List<School>()
+            {
+                new School{ Id = 1 , Name = "first school"},
+                new School{ Id = 2 , Name = "Second schools"}
+            };
+            return View(Schools);
+        }
+    }
+}
---
+++ CRUD/Models/School.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD.Models
+{
+    public class School
+    {
+        public int Id{ get; set; }
+        public string? Name { get; set; }
+
+    }
+}
